cac72c3b96926a98ffc8f8a03c3f20d60501b5f4
David Taylor <dktaylor1701@gmail.com>
2026-03-06 00:10:30 -0500

    Fix Nuthatch test isolation using ephemeral UserDefaults

--- Tests/NuthatchTests/UbiquitousKeyValueStoredTests.swift
+++ Tests/NuthatchTests/UbiquitousKeyValueStoredTests.swift
@@ -1,29 +1,39 @@
 import Testing
+import Foundation
 @testable import Nuthatch
 
 @Suite("UbiquitousKeyValueStored Tests")
 struct UbiquitousKeyValueStoredTests {
   @Test func defaultValue() {
-    var wrapper = UbiquitousKeyValueStored<Int>("test", defaultValue: 1)
+    let suiteName = UUID().uuidString
+    let userDefaults = UserDefaults(suiteName: suiteName)!
+    var wrapper = UbiquitousKeyValueStored<Int>("test", defaultValue: 1, userDefaults: userDefaults)
 
     wrapper.wrappedValue = nil
 
     #expect(wrapper.wrappedValue == 1)
+    userDefaults.removePersistentDomain(forName: suiteName)
   }
 
   @Test func nilDefaultValue() {
-    var wrapper = UbiquitousKeyValueStored<Int>("test", defaultValue: nil)
+    let suiteName = UUID().uuidString
+    let userDefaults = UserDefaults(suiteName: suiteName)!
+    var wrapper = UbiquitousKeyValueStored<Int>("test", defaultValue: nil, userDefaults: userDefaults)
 
     wrapper.wrappedValue = nil
 
     #expect(wrapper.wrappedValue == nil)
+    userDefaults.removePersistentDomain(forName: suiteName)
   }
 
   @Test func array() {
-    var wrapper = UbiquitousKeyValueStored<[Int]>("test", defaultValue: nil)
+    let suiteName = UUID().uuidString
+    let userDefaults = UserDefaults(suiteName: suiteName)!
+    var wrapper = UbiquitousKeyValueStored<[Int]>("test", defaultValue: nil, userDefaults: userDefaults)
 
     wrapper.wrappedValue = [1, 2, 3]
 
     #expect(wrapper.wrappedValue == [1, 2, 3])
+    userDefaults.removePersistentDomain(forName: suiteName)
   }
 }
